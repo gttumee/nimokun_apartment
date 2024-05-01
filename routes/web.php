057beab996a3c47903a390d32bcc6b854ccae565
tumee <tmkee0525@gmail.com>
2024-05-01 22:58:22 +0900

    add customer contact

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/qrcode',[QrCodeController::class,'generateQRCode']);
+Route::redirect('/', '/admin');
+
 
 Route::get('/', function () {
     return view('welcome');
