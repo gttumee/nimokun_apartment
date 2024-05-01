d395e57cdcb6900197ab8333b49a6ccc2f2c3bc4
tumee <tmkee0525@gmail.com>
2024-05-01 23:08:47 +0900

    service change and create

--- app/Filament/Resources/ServiceResource.php
+++ app/Filament/Resources/ServiceResource.php
@@ -6,9 +6,12 @@ use App\Filament\Resources\ServiceResource\Pages;
 use App\Filament\Resources\ServiceResource\RelationManagers;
 use App\Models\Service;
 use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -29,7 +32,16 @@ class ServiceResource extends Resource
     {
         return $form
             ->schema([
-                //
+                TextInput::make('name')->required()->label('事業名'),
+                TextInput::make('address')->required()->label('住所'),
+                TextInput::make('phone')->required()->label('連絡先'),
+                TextInput::make('contract')->required()->label('契約期間'),
+                Select::make('status')
+                ->options([
+                    '契約中' => '契約中',
+                    '契約終了' => '契約終了',
+                    '契約一時停止' => '契約一時停止',
+                ])->label('ステータス')->required(),
             ]);
     }
 
@@ -37,7 +49,21 @@ class ServiceResource extends Resource
     {
         return $table
             ->columns([
-                //
+                    TextColumn::make('name')->label('事業名')
+                    ->sortable()
+                    ->searchable(),
+                    TextColumn::make('address')->label('住所')
+                    ->sortable()
+                    ->searchable(),
+                    TextColumn::make('phone')->label('連絡先')
+                    ->sortable()
+                    ->searchable(),
+                    TextColumn::make('contract')->label('契約期間')
+                    ->sortable()
+                    ->searchable(),
+                    TextColumn::make('status')->label('ステータス')
+                    ->sortable()
+                    ->searchable(),
             ])
             ->filters([
                 //
--- app/Filament/Resources/ServiceResource/Pages/EditService.php
+++ app/Filament/Resources/ServiceResource/Pages/EditService.php
@@ -9,6 +9,10 @@ use Filament\Resources\Pages\EditRecord;
 class EditService extends EditRecord
 {
     protected static string $resource = ServiceResource::class;
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 
     protected function getHeaderActions(): array
     {
